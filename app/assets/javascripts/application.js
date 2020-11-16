856fb5cb6f971c55919cd72ab2ed0da7c1758c10
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2020-11-16 22:16:35 +0100

    Likes and followers and description box added and fixed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,12 +4,12 @@
 // that code so it'll be compiled.
 
 
+// require("jquery.min")
 // require("@rails/ujs").start()
 // require("turbolinks").start()
 // require("@rails/activestorage").start()
 // require("channels")
 // require("bootstrap.min")
-// require("jquery.min")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,12 +4,13 @@
 // // that code so it'll be compiled.
 
 
+require("jquery.min")
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("jquery.min")
 require("bootstrap.min")
+
 
 
 // // Uncomment to copy all static images under ../images to the output folder and reference
